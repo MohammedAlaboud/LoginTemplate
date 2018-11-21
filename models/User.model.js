f829188a1c9d956fe597c913540b79f5180cc01e
Mohammed Alaboud <mohammed.alaboud97@gmail.com>
2018-11-21 17:37:28 +0000

    User model Modified
    Added validations for user properties. Followed from mongoose docs

--- models/User.model.js
+++ models/User.model.js
@@ -1,13 +1,29 @@
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema; 
+var Schema = mongoose.Schema;
 
 
-//User properties
+//minlength from mongoose api; used SchemaString minlength
+//other validators exist that check for specific inputs
+var minlength = [3, 'The value of path `{PATH}` (`{VALUE}`) is shorter than the minimum allowed length ({minlength})' ];
+
+//User properties with validators 
 var UserSchema = new Schema({
-  username: String,
-  email: String,
-  password: String
+  username: {
+    type: String,
+    minlength: minlength,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true //only one person can sign up with this email!
+  },
+  password: {
+    type: String,
+  }
 });
 
-//to use in other files
+UserSchema.plugin(mongooseHidden); //setup plugin by passing in mon hidden param
+
+//to usei in other files
 module.exports = mongoose.model('User', UserSchema, 'users'); //reference db to fix issue for that data
